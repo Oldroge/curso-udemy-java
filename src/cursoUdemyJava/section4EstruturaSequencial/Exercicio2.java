3ba8bcab9f88771206d83c33467040a57a4c46ee
Oldroge <rogeriomrjr@gmail.com>
2023-05-03 10:35:23 -0300

    exercise: criar um programa que le o valor de um raio do circulo e imprime o valor da área

---
+++ src/cursoUdemyJava/section4EstruturaSequencial/Exercicio2.java
@@ -0,0 +1,28 @@
+package cursoUdemyJava.section4EstruturaSequencial;
+
+import java.util.Scanner;
+
+/**
+ * Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste
+ * círculo com quatro casas decimais conforme exemplos. Fórmula da área: area = π . raio2 Considere
+ * o valor de π = 3.14159
+ *
+ */
+
+public class Exercicio2 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    double pi = 3.14159;
+
+    double raio2 = sc.nextDouble();
+
+    double area = pi * Math.pow(raio2, 2);
+
+    System.out.printf("A=%.4f%n", area);
+
+    sc.close();
+  }
+
+}
