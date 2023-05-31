2475646cfe86f66dc2ed650126a3c11ccd7a6356
Oldroge <rogeriomrjr@gmail.com>
2023-05-31 16:25:42 -0300

    feat: entidade UsedProduct que herda de Product e sobrescreve método priceTag

---
+++ src/cursoUdemyJava/section14/exercise/entities/UsedProduct.java
@@ -0,0 +1,31 @@
+package cursoUdemyJava.section14.exercise.entities;
+
+import java.time.LocalDate;
+
+public class UsedProduct extends Product {
+  private LocalDate manufactureDate;
+
+  public UsedProduct() {
+    super();
+  }
+
+  public UsedProduct(String name, double price, LocalDate manufactureDate) {
+    super(name, price);
+    this.manufactureDate = manufactureDate;
+  }
+
+  public LocalDate getManufactureDate() {
+    return manufactureDate;
+  }
+
+  public void setManufactureDate(LocalDate manufactureDate) {
+    this.manufactureDate = manufactureDate;
+  }
+
+
+  @Override
+  public String priceTag() {
+    return "(used) $ " + super.getPrice() + "(Manufacture date: " + this.manufactureDate;
+  }
+
+}
